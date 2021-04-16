c24d4987e41151d8daeac6280c5d8bd97b67aeda
KLNXXX <79549429+KLNXXX@users.noreply.github.com>
2021-04-15 19:21:29 -0700

    Add files via upload

--- obfuscator.lua
+++ obfuscator.lua
@@ -2,5 +2,5 @@ local Code = [[
 	print("Hello!")
 ]]
 
-local Encoded = link:gsub(".", function(zen) return "\\" .. zen:byte() end) or link .. "\""
+local Encoded = Code:gsub(".", function(zen) return "\\" .. zen:byte() end) or Code .. "\""
 print(Encoded) -- this is the obfuscated code
